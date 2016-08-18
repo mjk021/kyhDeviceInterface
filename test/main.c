b1e1c34ee13a29bbb12847be4fa32f27b2766700
mjk021 <mjk021505@163.com>
2016-08-18 17:22:18 +0800

    test2

--- test/main.c
+++ test/main.c
@@ -13,10 +13,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/shm.h>  
-
 #include <sys/time.h>
 #include <pthread.h>
-
 #include <sys/shm.h>  
 #include <unistd.h>  
 #include <sys/types.h>  
